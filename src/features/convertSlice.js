56b88a12f3ea01c889a71c40b4712a0540285862
luiseer <85654418+luiseer@users.noreply.github.com>
2022-06-10 19:11:27 -0500

    [FEATURE] convert numbers

--- src/features/convertSlice.js
+++ src/features/convertSlice.js
@@ -1,20 +1,80 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from '@reduxjs/toolkit';
 
 export const convertSlice = createSlice({
   name: 'convert',
   initialState: {
-    value: '',
+    value: ''
   },
   reducers: {
-    romanToDecimal: (state) => {
-      state.value = '123456789';
-    },
-    decimalToRoman: (state) => {
-      state.value = '12345';
+    romanToDecimal: (state, action) => {
+      let payload = action.payload
+      console.log(payload);
+
+      const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
     }
-  },
-})
 
-export const { romanToDecimal, decimalToRoman } = convertSlice.actions
+      let number = 0
+      let last = 0
 
-export default convertSlice.reducer
+      for (let i = payload.length - 1; i >= 0; i--) {
+          const current = romanMap[payload[i]]
+          console.log(current);
+          if (current < last) {
+              number -= current
+          } else {
+              number += current
+          }
+          last = current
+          
+      }
+      state.value = number
+    },
+    
+    decimalToRoman: (state, action) => {
+
+      let payload = action.payload
+      console.log(payload);
+
+      let roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+      };
+
+      let str = '';
+
+      for (let i of Object.keys(roman)) {
+        
+        let j = Math.floor(payload / roman[i]);
+
+        payload -= j * roman[i];
+
+        str += i.repeat(j);
+        
+      }
+      state.value = str
+
+    }
+  }
+});
+
+export const { romanToDecimal, decimalToRoman } = convertSlice.actions;
+
+export default convertSlice.reducer;
